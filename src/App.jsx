abc4eb009e057c5bf40373aec1aa5b9dbc2446e9
uditi sharma <162996543+uditisharmaaa@users.noreply.github.com>
2025-09-13 22:01:25 -0400

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+// src/App.jsx
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "./pages/Home";
 import UserPage from "./pages/UserPage";
+import AdminPage from "./pages/AdminPage";
 import ProfilePage from "./pages/ProfilePage";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<UserPage />} />
+        {/* Landing: choose role */}
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+
+        {/* User flow */}
         <Route path="/user" element={<UserPage />} />
         <Route path="/profile" element={<ProfilePage />} />
+
+        {/* Admin flow */}
+        <Route path="/admin" element={<AdminPage />} />
+
+        {/* 404 */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
